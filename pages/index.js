f6a121499a577c09d989c9d2e1b83724161dc340
Anthony Morris <anthony@amorrissound.com>
2021-02-02 21:25:06 -0800

    Clean up the repo and install Tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,21 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="max-w-3xl mx-auto space-y-10">
+      <h1 className="font-bold text-7xl text-primary mt-10">bard.</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className="text-2xl space-y-2">
+        <p>Hey there! 👋</p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        <p>
+          We're working hard to make Bard into a thing that you'll want to use.
+          It's not there yet. If you have an idea of what you want in an online
+          publishing platform, let me know:{' '}
+          <a className="font-bold text-primary hover:cursor-pointer hover:underline">
+            amorriscode@gmail.com
+          </a>
+          .
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#004346',
+        secondary: '#81D2C7',
+        snow: '#FFFAFB',
+        platinum: '#E0E0E2',
+        gunmetal: '#292F36',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      cursor: ['hover'],
+    },
+  },
+  plugins: [],
+}
